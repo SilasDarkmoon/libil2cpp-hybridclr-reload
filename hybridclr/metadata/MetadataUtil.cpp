6f4e48b1abda00f95136a0afd01c7a9f6e289bd6
Silas <hdss3609@gmail.com>
2026-07-27 11:13:45 +0800

    try to locate Missing Method Exception

--- hybridclr/metadata/MetadataUtil.cpp
+++ hybridclr/metadata/MetadataUtil.cpp
@@ -704,8 +704,9 @@ namespace metadata
 				il2cpp::os::Directory::Create(dirStr, &createError);
 				FILE* fp = fopen((dirStr + "/assembly_reload_reuse.log").c_str(), "a");
 				if (fp) {
-					fprintf(fp, "[ReuseDiag] GetMethodInfoFromMethodDef FAIL: method='%s' methodDef=%p klass='%s.%s' method_count=%u klass_image=%p typeMetaHandle=%p type_typeHandle=%p\n",
+					fprintf(fp, "[ReuseDiag] GetMethodInfoFromMethodDef FAIL: method='%s' methodDef=%p klass_ptr=%p klass='%s.%s' method_count=%u klass_image=%p typeMetaHandle=%p type_typeHandle=%p\n",
 						mname, (void*)methodDef,
+						(void*)klass,
 						klass->namespaze ? klass->namespaze : "", klass->name ? klass->name : "",
 						(unsigned)klass->method_count,
 						(void*)klass->image, (void*)klass->typeMetadataHandle,
