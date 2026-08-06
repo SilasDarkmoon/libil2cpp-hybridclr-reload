607fbf524f6a2655890b9f0b9b914282e8331f4c
Silas <hdss3609@gmail.com>
2026-08-06 16:19:29 +0800

    try to locate old/new class mismatch

--- icalls/mscorlib/System/Delegate.cpp
+++ icalls/mscorlib/System/Delegate.cpp
@@ -9,9 +9,6 @@
 #include "vm/Reflection.h"
 #include "vm/Runtime.h"
 #include "vm/Type.h"
-#include <cstdio>
-#include "os/Directory.h"
-#include "os/Environment.h"
 
 namespace il2cpp
 {
@@ -25,57 +22,6 @@ namespace System
     {
         Il2CppClass *delegate_class = il2cpp::vm::Class::FromIl2CppType(__type->type);
         const MethodInfo *method = info->method;
-
-        // ==={{ AssemblyReloadReuse: diagnostic for Delegate.CreateDelegate
-        if (delegate_class && method && delegate_class->name && method->name &&
-            method->klass && method->klass->name)
-        {
-            static int s_logCount = 0;
-            if (s_logCount < 50)
-            {
-                s_logCount++;
-                const std::string tmpCache = il2cpp::os::Environment::GetEnvironmentVariable("UNITY_TEMPORARY_CACHE_PATH");
-                std::string dirStr = !tmpCache.empty() ? tmpCache : "log";
-                int createError = 0;
-                il2cpp::os::Directory::Create(dirStr, &createError);
-                FILE* fp = fopen((dirStr + "/assembly_reload_reuse.log").c_str(), "a");
-                if (fp)
-                {
-                    // Find Invoke method in delegate class
-                    il2cpp::vm::Class::Init(delegate_class);
-                    const MethodInfo* invokeMethod = nullptr;
-                    for (int i = 0; i < delegate_class->method_count; i++)
-                    {
-                        if (strcmp(delegate_class->methods[i]->name, "Invoke") == 0)
-                        {
-                            invokeMethod = delegate_class->methods[i];
-                            break;
-                        }
-                    }
-                    fprintf(fp, "[ReuseDiag] CreateDelegate_internal: delegate=%s method=%s.%s\n",
-                        delegate_class->name, method->klass->name, method->name);
-                    if (invokeMethod)
-                    {
-                        fprintf(fp, "  delegate Invoke: paramCount=%u\n", (unsigned)invokeMethod->parameters_count);
-                        for (uint16_t i = 0; i < invokeMethod->parameters_count && i < 6; i++)
-                        {
-                            const Il2CppType* pt = invokeMethod->parameters[i];
-                            if (pt)
-                                fprintf(fp, "    dParam[%u]: type=%u handle=%p\n", (unsigned)i, (unsigned)pt->type, (void*)pt->data.typeHandle);
-                        }
-                    }
-                    fprintf(fp, "  target method: paramCount=%u\n", (unsigned)method->parameters_count);
-                    for (uint16_t i = 0; i < method->parameters_count && i < 6; i++)
-                    {
-                        const Il2CppType* pt = method->parameters[i];
-                        if (pt)
-                            fprintf(fp, "    mParam[%u]: type=%u handle=%p\n", (unsigned)i, (unsigned)pt->type, (void*)pt->data.typeHandle);
-                    }
-                    fclose(fp);
-                }
-            }
-        }
-        // ===}} AssemblyReloadReuse
 
         IL2CPP_ASSERT(delegate_class->parent == il2cpp_defaults.multicastdelegate_class);
 
